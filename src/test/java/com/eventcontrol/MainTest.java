2ae973b3c5e3218d8b4c48e95c8060f7d03156f6
Zeroday456 <spageti456@gmail.com>
2026-06-29 12:01:27 +0300

    Добавлен package com.eventcontrol в оба файла

---
+++ src/test/java/com/eventcontrol/MainTest.java
@@ -0,0 +1,11 @@
+package com.eventcontrol;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    public void testAlwaysTrue() {
+        assertTrue(true);
+    }
+}
